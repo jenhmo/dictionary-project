0b77c52ec7474a774d3d93b9f9108254d39dd561
jenhmo <101249416+jenhmo@users.noreply.github.com>
2022-08-02 11:27:41 -0700

    included results for search

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,14 @@
 import axios from "axios";
+import Results from "./Results";
 import React, { useState } from "react";
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState("");
+    let [results, setResults] =useState(null);
 
     function handleResponse(response){
         console.log(response.data[0]);
+        setResults(response.data[0]);
     }
     function search(event) {
         event.preventDefault();
@@ -22,6 +25,7 @@ export default function Dictionary() {
             <form onSubmit={search}>
                 <input type="search" onChange={handleKeywordChange} />
                 </form>
+                <Results results={results} />
         </div>
     )
 }
